43ca6de6bda47fe48eb19252e52991f4b1ce2f80
giovanni <pino_otto@yahoo.com>
2017-07-23 15:50:47 +0800

    added license

--- firmware/esp8266/wbBasic.ino
+++ firmware/esp8266/wbBasic.ino
@@ -1,10 +1,27 @@
 /*
- * Basic IO
- * Use a pushbutton to toggle the onboard LED.
+Copyright (C) 2017 by Giovanni Di Mingo <pino_otto@yahoo.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+/*
+ * Use a pushbutton to toggle the onboard LED and send a HTTP request.
  *
  * If you do not have the 1 Button Shield, add a pushbutton or switch between D3 and GND.
  * 
- * This sketch sends data via HTTP GET requests
+ * This sketch sends data via HTTP GET requests.
  * 
  */
 
